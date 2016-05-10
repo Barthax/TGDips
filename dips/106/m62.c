dc59376bb3923ca865293e7a8bdef5222194325e
terencew <Mr.Terence.Wong@gmail.com>
2016-05-10 15:19:13 +1000

    106/m62.c: minor tidying, added game drivers info

--- dips/106/m62.c
+++ dips/106/m62.c
@@ -1,3 +1,94 @@
+#define IN0_PORT \
+/* Start 1 & 2 also restarts and freezes the game with stop mode on \
+   and are used in test mode to enter and esc the various tests */ \
+	PORT_BIT( 0x01, IP_ACTIVE_LOW, IPT_START1 ) \
+	PORT_BIT( 0x02, IP_ACTIVE_LOW, IPT_START2 ) \
+	/* service coin must be active for 19 frames to be consistently recognized */ \
+	PORT_BIT( 0x04, IP_ACTIVE_LOW, IPT_SERVICE1 ) PORT_IMPULSE(19) \
+	PORT_BIT( 0x08, IP_ACTIVE_LOW, IPT_COIN1 ) \
+	PORT_BIT( 0xf0, IP_ACTIVE_LOW, IPT_UNUSED )
+
+#define IN1_PORT \
+	PORT_BIT( 0x01, IP_ACTIVE_LOW, IPT_JOYSTICK_RIGHT ) PORT_8WAY \
+	PORT_BIT( 0x02, IP_ACTIVE_LOW, IPT_JOYSTICK_LEFT ) PORT_8WAY \
+	PORT_BIT( 0x04, IP_ACTIVE_LOW, IPT_JOYSTICK_DOWN ) PORT_8WAY \
+	PORT_BIT( 0x08, IP_ACTIVE_LOW, IPT_JOYSTICK_UP ) PORT_8WAY \
+	PORT_BIT( 0x10, IP_ACTIVE_LOW, IPT_UNKNOWN ) /* probably unused */ \
+	PORT_BIT( 0x20, IP_ACTIVE_LOW, IPT_BUTTON2 ) \
+	PORT_BIT( 0x40, IP_ACTIVE_LOW, IPT_UNKNOWN ) /* probably unused */ \
+	PORT_BIT( 0x80, IP_ACTIVE_LOW, IPT_BUTTON1 )
+
+#define IN2_PORT \
+	PORT_BIT( 0x01, IP_ACTIVE_LOW, IPT_JOYSTICK_RIGHT ) PORT_8WAY PORT_COCKTAIL \
+	PORT_BIT( 0x02, IP_ACTIVE_LOW, IPT_JOYSTICK_LEFT ) PORT_8WAY PORT_COCKTAIL \
+	PORT_BIT( 0x04, IP_ACTIVE_LOW, IPT_JOYSTICK_DOWN ) PORT_8WAY PORT_COCKTAIL \
+	PORT_BIT( 0x08, IP_ACTIVE_LOW, IPT_JOYSTICK_UP ) PORT_8WAY PORT_COCKTAIL \
+	PORT_BIT( 0x10, IP_ACTIVE_LOW, IPT_COIN2 ) \
+	PORT_BIT( 0x20, IP_ACTIVE_LOW, IPT_BUTTON2 ) PORT_COCKTAIL \
+	PORT_BIT( 0x40, IP_ACTIVE_LOW, IPT_UNKNOWN ) /* probably unused */ \
+	PORT_BIT( 0x80, IP_ACTIVE_LOW, IPT_BUTTON1 ) PORT_COCKTAIL
+
+#define	COINAGE_DSW \
+	/* TODO: support the different settings which happen in Coin Mode 2 */ \
+	PORT_DIPNAME( 0xf0, 0xf0, DEF_STR( Coinage ) ) /* mapped on coin mode 1 */ \
+	PORT_DIPSETTING(    0x90, DEF_STR( 7C_1C ) ) \
+	PORT_DIPSETTING(    0xa0, DEF_STR( 6C_1C ) ) \
+	PORT_DIPSETTING(    0xb0, DEF_STR( 5C_1C ) ) \
+	PORT_DIPSETTING(    0xc0, DEF_STR( 4C_1C ) ) \
+	PORT_DIPSETTING(    0xd0, DEF_STR( 3C_1C ) ) \
+	PORT_DIPSETTING(    0xe0, DEF_STR( 2C_1C ) ) \
+	PORT_DIPSETTING(    0xf0, DEF_STR( 1C_1C ) ) \
+	PORT_DIPSETTING(    0x70, DEF_STR( 1C_2C ) ) \
+	PORT_DIPSETTING(    0x60, DEF_STR( 1C_3C ) ) \
+	PORT_DIPSETTING(    0x50, DEF_STR( 1C_4C ) ) \
+	PORT_DIPSETTING(    0x40, DEF_STR( 1C_5C ) ) \
+	PORT_DIPSETTING(    0x30, DEF_STR( 1C_6C ) ) \
+	PORT_DIPSETTING(    0x20, DEF_STR( 1C_7C ) ) \
+	PORT_DIPSETTING(    0x10, DEF_STR( 1C_8C ) ) \
+	PORT_DIPSETTING(    0x00, DEF_STR( Free_Play ) ) \
+	/* setting 0x80 give 1 Coin/1 Credit */
+
+#define	COINAGE2_DSW \
+	/* TODO: support the different settings which happen in Coin Mode 2 */ \
+	PORT_DIPNAME( 0xf0, 0xf0, DEF_STR( Coinage ) ) /* mapped on coin mode 1 */ \
+	PORT_DIPSETTING(    0xa0, DEF_STR( 6C_1C ) ) \
+	PORT_DIPSETTING(    0xb0, DEF_STR( 5C_1C ) ) \
+	PORT_DIPSETTING(    0xc0, DEF_STR( 4C_1C ) ) \
+	PORT_DIPSETTING(    0xd0, DEF_STR( 3C_1C ) ) \
+	PORT_DIPSETTING(    0x10, DEF_STR( 8C_3C ) ) \
+	PORT_DIPSETTING(    0xe0, DEF_STR( 2C_1C ) ) \
+	PORT_DIPSETTING(    0x20, DEF_STR( 5C_3C ) ) \
+	PORT_DIPSETTING(    0x30, DEF_STR( 3C_2C ) ) \
+	PORT_DIPSETTING(    0xf0, DEF_STR( 1C_1C ) ) \
+	PORT_DIPSETTING(    0x40, DEF_STR( 2C_3C ) ) \
+	PORT_DIPSETTING(    0x90, DEF_STR( 1C_2C ) ) \
+	PORT_DIPSETTING(    0x80, DEF_STR( 1C_3C ) ) \
+	PORT_DIPSETTING(    0x70, DEF_STR( 1C_4C ) ) \
+	PORT_DIPSETTING(    0x60, DEF_STR( 1C_5C ) ) \
+	PORT_DIPSETTING(    0x50, DEF_STR( 1C_6C ) ) \
+	PORT_DIPSETTING(    0x00, DEF_STR( Free_Play ) ) \
+
+#define	COINAGE3_DSW \
+	/* TODO: support the different settings which happen in Coin Mode 2 */ \
+	PORT_DIPNAME( 0xf0, 0xf0, DEF_STR( Coinage ) ) /* mapped on coin mode 1 */ \
+	PORT_DIPSETTING(    0x00, DEF_STR( 8C_1C ) ) \
+	PORT_DIPSETTING(    0xa0, DEF_STR( 6C_1C ) ) \
+	PORT_DIPSETTING(    0xb0, DEF_STR( 5C_1C ) ) \
+	PORT_DIPSETTING(    0xc0, DEF_STR( 4C_1C ) ) \
+	PORT_DIPSETTING(    0xd0, DEF_STR( 3C_1C ) ) \
+	PORT_DIPSETTING(    0xe0, DEF_STR( 2C_1C ) ) \
+	PORT_DIPSETTING(    0x10, DEF_STR( 5C_3C ) ) \
+	PORT_DIPSETTING(    0x20, "3 Coins/5 Credits" ) \
+	PORT_DIPSETTING(    0x30, DEF_STR( 3C_2C ) ) \
+	PORT_DIPSETTING(    0xf0, DEF_STR( 1C_1C ) ) \
+	PORT_DIPSETTING(    0x40, DEF_STR( 2C_3C ) ) \
+	PORT_DIPSETTING(    0x90, DEF_STR( 1C_2C ) ) \
+	PORT_DIPSETTING(    0x80, DEF_STR( 1C_3C ) ) \
+	PORT_DIPSETTING(    0x70, DEF_STR( 1C_4C ) ) \
+	PORT_DIPSETTING(    0x60, DEF_STR( 1C_5C ) ) \
+	PORT_DIPSETTING(    0x50, DEF_STR( 1C_6C ) ) \
+
+
 INPUT_PORTS_START( kungfum )
 	PORT_START_TAG("IN0")
 	IN0_PORT
@@ -629,93 +720,26 @@ INPUT_PORTS_START( horizon )
 	PORT_SERVICE( 0x80, IP_ACTIVE_LOW )
 INPUT_PORTS_END
 
-#define IN0_PORT \
-/* Start 1 & 2 also restarts and freezes the game with stop mode on \
-   and are used in test mode to enter and esc the various tests */ \
-	PORT_BIT( 0x01, IP_ACTIVE_LOW, IPT_START1 ) \
-	PORT_BIT( 0x02, IP_ACTIVE_LOW, IPT_START2 ) \
-	/* service coin must be active for 19 frames to be consistently recognized */ \
-	PORT_BIT( 0x04, IP_ACTIVE_LOW, IPT_SERVICE1 ) PORT_IMPULSE(19) \
-	PORT_BIT( 0x08, IP_ACTIVE_LOW, IPT_COIN1 ) \
-	PORT_BIT( 0xf0, IP_ACTIVE_LOW, IPT_UNUSED )
 
-#define IN1_PORT \
-	PORT_BIT( 0x01, IP_ACTIVE_LOW, IPT_JOYSTICK_RIGHT ) PORT_8WAY \
-	PORT_BIT( 0x02, IP_ACTIVE_LOW, IPT_JOYSTICK_LEFT ) PORT_8WAY \
-	PORT_BIT( 0x04, IP_ACTIVE_LOW, IPT_JOYSTICK_DOWN ) PORT_8WAY \
-	PORT_BIT( 0x08, IP_ACTIVE_LOW, IPT_JOYSTICK_UP ) PORT_8WAY \
-	PORT_BIT( 0x10, IP_ACTIVE_LOW, IPT_UNKNOWN ) /* probably unused */ \
-	PORT_BIT( 0x20, IP_ACTIVE_LOW, IPT_BUTTON2 ) \
-	PORT_BIT( 0x40, IP_ACTIVE_LOW, IPT_UNKNOWN ) /* probably unused */ \
-	PORT_BIT( 0x80, IP_ACTIVE_LOW, IPT_BUTTON1 )
-
-#define IN2_PORT \
-	PORT_BIT( 0x01, IP_ACTIVE_LOW, IPT_JOYSTICK_RIGHT ) PORT_8WAY PORT_COCKTAIL \
-	PORT_BIT( 0x02, IP_ACTIVE_LOW, IPT_JOYSTICK_LEFT ) PORT_8WAY PORT_COCKTAIL \
-	PORT_BIT( 0x04, IP_ACTIVE_LOW, IPT_JOYSTICK_DOWN ) PORT_8WAY PORT_COCKTAIL \
-	PORT_BIT( 0x08, IP_ACTIVE_LOW, IPT_JOYSTICK_UP ) PORT_8WAY PORT_COCKTAIL \
-	PORT_BIT( 0x10, IP_ACTIVE_LOW, IPT_COIN2 ) \
-	PORT_BIT( 0x20, IP_ACTIVE_LOW, IPT_BUTTON2 ) PORT_COCKTAIL \
-	PORT_BIT( 0x40, IP_ACTIVE_LOW, IPT_UNKNOWN ) /* probably unused */ \
-	PORT_BIT( 0x80, IP_ACTIVE_LOW, IPT_BUTTON1 ) PORT_COCKTAIL
-
-#define	COINAGE_DSW \
-	/* TODO: support the different settings which happen in Coin Mode 2 */ \
-	PORT_DIPNAME( 0xf0, 0xf0, DEF_STR( Coinage ) ) /* mapped on coin mode 1 */ \
-	PORT_DIPSETTING(    0x90, DEF_STR( 7C_1C ) ) \
-	PORT_DIPSETTING(    0xa0, DEF_STR( 6C_1C ) ) \
-	PORT_DIPSETTING(    0xb0, DEF_STR( 5C_1C ) ) \
-	PORT_DIPSETTING(    0xc0, DEF_STR( 4C_1C ) ) \
-	PORT_DIPSETTING(    0xd0, DEF_STR( 3C_1C ) ) \
-	PORT_DIPSETTING(    0xe0, DEF_STR( 2C_1C ) ) \
-	PORT_DIPSETTING(    0xf0, DEF_STR( 1C_1C ) ) \
-	PORT_DIPSETTING(    0x70, DEF_STR( 1C_2C ) ) \
-	PORT_DIPSETTING(    0x60, DEF_STR( 1C_3C ) ) \
-	PORT_DIPSETTING(    0x50, DEF_STR( 1C_4C ) ) \
-	PORT_DIPSETTING(    0x40, DEF_STR( 1C_5C ) ) \
-	PORT_DIPSETTING(    0x30, DEF_STR( 1C_6C ) ) \
-	PORT_DIPSETTING(    0x20, DEF_STR( 1C_7C ) ) \
-	PORT_DIPSETTING(    0x10, DEF_STR( 1C_8C ) ) \
-	PORT_DIPSETTING(    0x00, DEF_STR( Free_Play ) ) \
-	/* setting 0x80 give 1 Coin/1 Credit */
-
-#define	COINAGE2_DSW \
-	/* TODO: support the different settings which happen in Coin Mode 2 */ \
-	PORT_DIPNAME( 0xf0, 0xf0, DEF_STR( Coinage ) ) /* mapped on coin mode 1 */ \
-	PORT_DIPSETTING(    0xa0, DEF_STR( 6C_1C ) ) \
-	PORT_DIPSETTING(    0xb0, DEF_STR( 5C_1C ) ) \
-	PORT_DIPSETTING(    0xc0, DEF_STR( 4C_1C ) ) \
-	PORT_DIPSETTING(    0xd0, DEF_STR( 3C_1C ) ) \
-	PORT_DIPSETTING(    0x10, DEF_STR( 8C_3C ) ) \
-	PORT_DIPSETTING(    0xe0, DEF_STR( 2C_1C ) ) \
-	PORT_DIPSETTING(    0x20, DEF_STR( 5C_3C ) ) \
-	PORT_DIPSETTING(    0x30, DEF_STR( 3C_2C ) ) \
-	PORT_DIPSETTING(    0xf0, DEF_STR( 1C_1C ) ) \
-	PORT_DIPSETTING(    0x40, DEF_STR( 2C_3C ) ) \
-	PORT_DIPSETTING(    0x90, DEF_STR( 1C_2C ) ) \
-	PORT_DIPSETTING(    0x80, DEF_STR( 1C_3C ) ) \
-	PORT_DIPSETTING(    0x70, DEF_STR( 1C_4C ) ) \
-	PORT_DIPSETTING(    0x60, DEF_STR( 1C_5C ) ) \
-	PORT_DIPSETTING(    0x50, DEF_STR( 1C_6C ) ) \
-	PORT_DIPSETTING(    0x00, DEF_STR( Free_Play ) ) \
-
-#define	COINAGE3_DSW \
-	/* TODO: support the different settings which happen in Coin Mode 2 */ \
-	PORT_DIPNAME( 0xf0, 0xf0, DEF_STR( Coinage ) ) /* mapped on coin mode 1 */ \
-	PORT_DIPSETTING(    0x00, DEF_STR( 8C_1C ) ) \
-	PORT_DIPSETTING(    0xa0, DEF_STR( 6C_1C ) ) \
-	PORT_DIPSETTING(    0xb0, DEF_STR( 5C_1C ) ) \
-	PORT_DIPSETTING(    0xc0, DEF_STR( 4C_1C ) ) \
-	PORT_DIPSETTING(    0xd0, DEF_STR( 3C_1C ) ) \
-	PORT_DIPSETTING(    0xe0, DEF_STR( 2C_1C ) ) \
-	PORT_DIPSETTING(    0x10, DEF_STR( 5C_3C ) ) \
-	PORT_DIPSETTING(    0x20, "3 Coins/5 Credits" ) \
-	PORT_DIPSETTING(    0x30, DEF_STR( 3C_2C ) ) \
-	PORT_DIPSETTING(    0xf0, DEF_STR( 1C_1C ) ) \
-	PORT_DIPSETTING(    0x40, DEF_STR( 2C_3C ) ) \
-	PORT_DIPSETTING(    0x90, DEF_STR( 1C_2C ) ) \
-	PORT_DIPSETTING(    0x80, DEF_STR( 1C_3C ) ) \
-	PORT_DIPSETTING(    0x70, DEF_STR( 1C_4C ) ) \
-	PORT_DIPSETTING(    0x60, DEF_STR( 1C_5C ) ) \
-	PORT_DIPSETTING(    0x50, DEF_STR( 1C_6C ) ) \
-
+GAME( 1984, kungfum,  0,        kungfum,  kungfum,  0,        ROT0,   "Irem", "Kung-Fu Master", 0 )
+GAME( 1984, kungfud,  kungfum,  kungfum,  kungfum,  0,        ROT0,   "Irem (Data East license)", "Kung-Fu Master (Data East)", 0 )
+GAME( 1984, spartanx, kungfum,  kungfum,  kungfum,  0,        ROT0,   "Irem", "Spartan X (Japan)", 0 )
+GAME( 1984, kungfub,  kungfum,  kungfum,  kungfum,  0,        ROT0,   "bootleg", "Kung-Fu Master (bootleg set 1)", 0 )
+GAME( 1984, kungfub2, kungfum,  kungfum,  kungfum,  0,        ROT0,   "bootleg", "Kung-Fu Master (bootleg set 2)", 0 )
+GAME( 1984, battroad, 0,        battroad, battroad, battroad, ROT90,  "Irem", "The Battle-Road", 0 )
+GAME( 1984, ldrun,    0,        ldrun,    ldrun,    0,        ROT0,   "Irem (licensed from Broderbund)", "Lode Runner (set 1)", 0 )
+GAME( 1984, ldruna,   ldrun,    ldrun,    ldrun,    0,        ROT0,   "Irem (licensed from Broderbund)", "Lode Runner (set 2)", 0 )
+GAME( 1984, ldrun2,   0,        ldrun2,   ldrun2,   ldrun2,   ROT0,   "Irem (licensed from Broderbund)", "Lode Runner II - The Bungeling Strikes Back" , 0)	/* Japanese version is called Bangeringu Teikoku No Gyakushuu */
+GAME( 1985, ldrun3,   0,        ldrun3,   ldrun3,   0,        ROT0,   "Irem (licensed from Broderbund)", "Lode Runner III - The Golden Labyrinth", 0 )
+GAME( 1985, ldrun3jp, ldrun3,   ldrun3,   ldrun3,   0,        ROT0,   "Irem (licensed from Broderbund)", "Lode Runner III - Majin No Fukkatsu", 0 )
+GAME( 1986, ldrun4,   0,        ldrun4,   ldrun4,   ldrun4,   ROT0,   "Irem (licensed from Broderbund)", "Lode Runner IV - Teikoku Karano Dasshutsu", 0 )
+GAME( 1985, lotlot,   0,        lotlot,   lotlot,   0,        ROT0,   "Irem (licensed from Tokuma Shoten)", "Lot Lot", 0 )
+GAME( 1986, kidniki,  0,        kidniki,  kidniki,  kidniki,  ROT0,   "Irem", "Kid Niki - Radical Ninja (World)", GAME_IMPERFECT_SOUND )
+GAME( 1986, kidnikiu, kidniki,  kidniki,  kidniki,  kidniki,  ROT0,   "Irem (Data East USA license)", "Kid Niki - Radical Ninja (US)", GAME_IMPERFECT_SOUND )
+GAME( 1986, yanchamr, kidniki,  kidniki,  kidniki,  kidniki,  ROT0,   "Irem", "Kaiketsu Yanchamaru (Japan)", GAME_IMPERFECT_SOUND )
+GAME( 1987, lithero,  kidniki,  kidniki,  kidniki,  kidniki,  ROT0,   "bootleg", "Little Hero", GAME_IMPERFECT_SOUND )
+GAME( 1985, spelunkr, 0,        spelunkr, spelunkr, spelunkr, ROT0,   "Irem (licensed from Broderbund)", "Spelunker", 0 )
+GAME( 1985, spelnkrj, spelunkr, spelunkr, spelunkr, spelunkr, ROT0,   "Irem (licensed from Broderbund)", "Spelunker (Japan)", 0 )
+GAME( 1986, spelunk2, 0,        spelunk2, spelunk2, spelunk2, ROT0,   "Irem (licensed from Broderbund)", "Spelunker II", 0 )
+GAME( 1986, youjyudn, 0,        youjyudn, youjyudn, youjyudn, ROT270, "Irem", "Youjyuden (Japan)", 0 )
+GAME( 1985, horizon,  0,        horizon,  horizon,  0,        ROT0,   "Irem", "Horizon", GAME_IMPERFECT_SOUND )
